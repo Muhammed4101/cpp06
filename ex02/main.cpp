32fe627804b114a14e1f942b580e58fd6e8da6ae
Muhammed <muhammedarslangs41@gmail.com>
2026-05-17 14:36:27 +0300

    completed

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,22 +9,28 @@ int main()
 
     std::cout << "--- TEST 1 ---" << std::endl;
     Base* obj1 = generate();
-    std::cout << "Pointer ID : "; identify(obj1);
-    std::cout << "Reference ID: "; identify(*obj1);
+    std::cout << "Pointer ID : "; 
+    identify(obj1);
+    std::cout << "Reference ID: ";
+    identify(*obj1);
     delete obj1;
     std::cout << std::endl;
 
     std::cout << "--- TEST 2 ---" << std::endl;
     Base* obj2 = generate();
-    std::cout << "Pointer ID : "; identify(obj2);
-    std::cout << "Reference ID: "; identify(*obj2);
+    std::cout << "Pointer ID : "; 
+    identify(obj2);
+    std::cout << "Reference ID: ";
+    identify(*obj2);
     delete obj2;
     std::cout << std::endl;
 
     std::cout << "--- TEST 3 ---" << std::endl;
     Base* obj3 = generate();
-    std::cout << "Pointer ID : "; identify(obj3);
-    std::cout << "Reference ID: "; identify(*obj3);
+    std::cout << "Pointer ID : "; 
+    identify(obj3);
+    std::cout << "Reference ID: ";
+    identify(*obj3);
     delete obj3;
     std::cout << std::endl;
 
